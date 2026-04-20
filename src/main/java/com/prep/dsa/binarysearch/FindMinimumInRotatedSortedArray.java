d16d1ac92939a14506a06be17f1340e81e8d69be
Abhishek Gupta <abhigupta7049612180@gmail.com>
2026-04-21 02:30:28 +0530

    Binary search questions added

---
+++ src/main/java/com/prep/dsa/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,64 @@
+package com.prep.dsa.binarysearch;
+
+public class FindMinimumInRotatedSortedArray {
+    /*
+    Find minimum in Rotated Sorted Array
+
+        Given an integer array nums of size N, sorted in ascending order with distinct values, and then rotated an unknown number of times (between 1 and N), find the minimum element in the array.
+
+
+            Example 1
+            Input : nums = [4, 5, 6, 7, 0, 1, 2, 3]
+            Output: 0
+            Explanation: Here, the element 0 is the minimum element in the array.
+
+            Example 2
+            Input : nums = [3, 4, 5, 1, 2]
+            Output: 1
+            Explanation:Here, the element 1 is the minimum element in the array.
+    */
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5,6,7,8, 1, 2};
+        int ans = findMinimumInRotatedArray(arr);
+        System.out.println("Minimum: "+ans);
+    }
+
+    private static int findMinimumInRotatedArray(int[] arr) {
+        int ans = Integer.MAX_VALUE;
+        int low = 0, high = arr.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            //optional optimization
+            // if search space is already sorted, then always arr[low] is smaller element in that search space,
+            if(arr[low]<= arr[high]){
+                ans = Math.min(ans, arr[low]);
+                break;
+            }
+
+            if(arr[low]<= arr[mid]){
+                ans = Math.min(arr[low], ans);
+                low = mid+1;
+            }else{
+                ans = Math.min(arr[mid], ans);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    /*
+    * Approach -->
+    *
+    * As we know our sorted array rotated, if we left half is sorted it means
+    * if we use sorted property so left half i.e(low -> mid) arr[low] is smaller.
+    * and we can take arr[low] is a possible answer, and we can eliminate left half because
+    * we already choose element from there, and change low as low = mid+1
+    *
+    * if right half is sorted i.e(mid -> high) then means arr[mid] will be smaller and possible answer
+    * so we can choose and eliminate right half and change high as high = mid -1
+    *
+    * and keep running this process, till high not crosses low
+    *
+    *  */
+}
---
+++ src/main/java/com/prep/dsa/binarysearch/SearchElementInRotatedArray.java
@@ -0,0 +1,97 @@
+package com.prep.dsa.binarysearch;
+
+public class SearchElementInRotatedArray {
+    /*
+    Search in rotated sorted array-I
+
+Given an integer array nums, sorted in ascending order (with distinct values) and a target value k. The array is rotated at some pivot point that is unknown. Find the index at which k is present and if k is not present return -1.
+
+        Example 1
+        Input : nums = [4, 5, 6, 7, 0, 1, 2], k = 0
+        Output: 4
+
+        Explanation: Here, the target is 0. We can see that 0 is present in the given rotated sorted array, nums. Thus, we get output as 4, which is the index at which 0 is present in the array.
+
+        Example 2
+        Input: nums = [4, 5, 6, 7, 0, 1, 2], k = 3
+        Output: -1
+
+        Explanation: Here, the target is 3. Since 3 is not present in the given rotated sorted array. Thus, we get the output as -1.
+
+    */
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2}; int num = 4;
+        int ans = searchElement(arr,num);
+        System.out.println("Position: "+ans);
+
+        //Duplicate values case
+        int[] arrWithDuplicate = {3,3,3,4,3,3,1,2,3,3,3,3}; num = 4;
+        ans = searchElement_Duplicate_Allowed(arrWithDuplicate,num);
+        System.out.println("Duplicate case --> Position: "+ans);
+
+    }
+
+    private static int searchElement(int[] arr, int num) {
+        int low = 0, high = arr.length-1;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==num)return mid;
+            if(arr[low] <= arr[mid]){
+                if(num >= arr[low] && num <= arr[mid]){
+                    high = mid-1;
+                }else
+                    low = mid+1;
+            }else{
+                if(num >= arr[mid] && num <= arr[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    /* Approach -->
+    * Because array is rotated, when we calculate the mid, if mid is not our target then either left half
+    * is sorted or right half will be sorted.
+    *
+    * how we'll get to know because of the sorted property if arr[low] is smaller or equals to arr[mid]
+    * means left half is sorted
+    *
+    * if arr[mid] is small or equals to arr[high] then right half is sorted.
+    *
+    * after identification, we need to eliminate left or right half, we can do that
+    * if left half is sorted, then we'll check target is lying between low and mid, if yes then we'll eliminate right half
+    * if not then eliminate, left half
+    *
+    * if right is sorted, then we'll check our target is lying in right half or not
+    * if yes then we'll change the low = mid+1 and eliminate left half
+    * if no means high = mid-1 and eliminate right half.
+    *
+    * */
+
+    private static int searchElement_Duplicate_Allowed(int[] arr, int num) {
+        int low = 0, high = arr.length-1;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==num)return mid;
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                low++; high--;
+                continue;
+            }
+            if(arr[low] <= arr[mid]){
+                if(num >= arr[low] && num <= arr[mid]){
+                    high = mid-1;
+                }else
+                    low = mid+1;
+            }else{
+                if(num >= arr[mid] && num <= arr[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
